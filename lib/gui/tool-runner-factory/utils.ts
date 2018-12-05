448d7615e47ef288953a66ff634c32c7f101f6bc
Dfurzikova <dfurzikova@yandex.ru>
2018-12-05 18:20:50 +0300

    refactor: utils

--- lib/gui/tool-runner-factory/utils.ts
+++ lib/gui/tool-runner-factory/utils.ts
@@ -1,9 +1,9 @@
-'use strict';
+import { ITestTool } from 'typings/test-adapter';
 
 const _ = require('lodash');
 const {findNode} = require('../../static/modules/utils');
 
-const formatTestHandler = (browser, test) => {
+const formatTestHandler = (browser: { [key: string]: any}, test: ITestTool) => {
     const {suitePath, name} = test;
 
     return {
@@ -13,19 +13,19 @@ const formatTestHandler = (browser, test) => {
     };
 };
 
-exports.formatTests = (test) => {
+exports.formatTests = (test: ITestTool) => {
     if (test.children) {
-        return _.flatMap(test.children, (child) => exports.formatTests(child));
+        return _.flatMap(test.children, (child: ITestTool) => exports.formatTests(child));
     }
 
     if (test.browserId) {
         test.browsers = _.filter(test.browsers, {name: test.browserId});
     }
 
-    return _.flatMap(test.browsers, (browser) => formatTestHandler(browser, test));
+    return _.flatMap(test.browsers, (browser: { [key: string]: any}) => formatTestHandler(browser, test));
 };
 
-exports.findTestResult = (suites = [], test) => {
+exports.findTestResult = (suites = [], test: ITestTool) => {
     const {name, suitePath, browserId} = test;
     const nodeResult = findNode(suites, suitePath);
     const browserResult = _.find(nodeResult.browsers, {name: browserId});
