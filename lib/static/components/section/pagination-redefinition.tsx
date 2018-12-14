68b88612f19832ee5fd77827118f391806c3058a
Dfurzikova <dfurzikova@yandex.ru>
2018-12-14 16:00:10 +0300

    fix: add types

--- lib/static/components/section/pagination-redefinition.tsx
+++ lib/static/components/section/pagination-redefinition.tsx
@@ -5,14 +5,14 @@ export default class PaginationExtended extends Pagination{
         value: any;
     }) => void;
 
-    handleItemOverrides = (active: any, type: any, value: any) => (predefinedProps: any) => {
+    handleItemOverrides = (active: boolean, type: string, value: any) => (predefinedProps: any) => {
         const { retries } = this.props;
         return {
             active,
             type,
             className: retries[value] && type === 'pageItem' && retries[value].status ? retries[value].status : null ,
             key: `${type}-${value}`,
-            onClick: (e: any, itemProps: any) => {
+            onClick: (e: Event, itemProps: any) => {
                 _.invoke(predefinedProps, 'onClick', e, itemProps);
                 this.handleItemClick(e, itemProps);
             },
