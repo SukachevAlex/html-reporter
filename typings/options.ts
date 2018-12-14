a09d1f4b4735070e326a114e335c70fc5fa9357e
ilya777grin23 <IRuss23@yandex.ru>
2018-12-14 11:26:37 +0300

    refactor: migrate to ts - /lib/gui/*.js

--- typings/options.ts
+++ typings/options.ts
@@ -1,6 +1,7 @@
 export interface IOptions {
     enabled?: boolean;
     browserId?: string;
+    program?: any;
     baseHost?: string;
     forBrowser?(path: string): { getAbsoluteUrl(path: string): string };
 }
