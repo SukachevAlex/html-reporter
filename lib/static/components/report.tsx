7d41fb19699ee900d40bec7a2546b5af2fd8d299
Dfurzikova <dfurzikova@yandex.ru>
2018-12-17 12:22:04 +0300

    feat: add logo in static report

--- lib/static/components/report.tsx
+++ lib/static/components/report.tsx
@@ -3,6 +3,8 @@ import Summary from './summary/index';
 import SkippedList from './skipped-list';
 import Suites from './suites';
 import MenuControls from './controls/menu-controls';
+import { cn } from '@bem-react/classname';
+const cnLogo = cn('Logo');
 
 export default class Report extends Component {
     render() {
@@ -10,6 +12,7 @@ export default class Report extends Component {
             <MenuControls/>
             <div className='Container'>
                 <Summary/>
+                <img className={cnLogo()} src='./logo.png' />
                 <SkippedList/>
                 <Suites/>
             </div>
