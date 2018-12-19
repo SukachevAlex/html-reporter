9dce353541537b9e4d89ae4fe0fb806cd9717037
ilya777grin23 <IRuss23@yandex.ru>
2018-12-19 17:11:54 +0300

    fix: delete border and outline

--- lib/static/components/suites.tsx
+++ lib/static/components/suites.tsx
@@ -95,9 +95,11 @@ class Suites extends Component<ISuitesProps> {
                 {({measure}) => {
                     this.measure = measure.bind(this);
 
-                    return <div style={style} onLoad={measure}>
-                        <SectionCommon handler={this.clearAll} isRoot={true} suiteId={suiteIds[index]} />
-                    </div>;
+                    return (
+                        <div style={{padding: '1px', ...style}} onLoad={measure}>
+                            <SectionCommon handler={this.clearAll} isRoot={true} suiteId={suiteIds[index]} />
+                        </div>
+                    );
                 }}
             </CellMeasurer>
         );
@@ -107,9 +109,10 @@ class Suites extends Component<ISuitesProps> {
         const {suiteIds = []} = this.props;
 
         return (
-            <AutoSizer className='sections' style={{height: 'calc(100vh - 240px)'}}>
+            <AutoSizer className='Sections' style={{height: 'calc(100vh - 240px)'}}>
                 {({width, height}: any) =>
                     <List
+                        style={{outline: 'none', border: 'none'}}
                         deferredMeasurementCache={this.cache}
                         width={width}
                         height={height}
